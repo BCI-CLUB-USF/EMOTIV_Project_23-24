76f513a69249b38f60716e7a534291429eef6d06
Eloy Fernandes Ballesteros <46434706+eloyball@users.noreply.github.com>
2024-01-30 20:18:15 -0500

    Wifi Update

--- BCI_Motor_Test.ino
+++ BCI_Motor_Test.ino
@@ -4,169 +4,154 @@
  * \___/(___/ \___/ \__  |\___/ \___(_)____)___/|_|_|_|
  *                  (____/ 
  * Arduino Mecanum Omni Direction Wheel Robot Car
- * Tutorial URL https://osoyoo.com/?p=49235
+ * Tutorial URL http://osoyoo.com/?p=49235
  * CopyRight www.osoyoo.com
-
- * After running the code, smart car will 
- * go forward and go backward for 2 seconds, 
- * left turn and right turn for 2 seconds, 
- * right shift and left shift for 2 seconds,
- * left diagonal back and right diagonal forward for 2 seconds,
- * left diagonal forward and right diagonal back for 2 seconds,
- * then stop. 
+ *  
+ * In this project we will connect Robot Car to Wifi and Use 
+ * an APP to control the car through Internet. 
  * 
  */
-#define SPEED 25    
-#define TURN_SPEED 25 
+#define SPEED 80    
+#define TURN_SPEED 80 
+#define SHIFT_SPEED 80  
 
-#define speedPinR 52   //  Front Wheel PWM pin connect Model-Y M_B ENA 
+#define TURN_TIME 500  
+#define MOVE_TIME 500  
+
+#define speedPinR 10   //  Front Wheel PWM pin connect Model-Y M_B ENA 
 #define RightMotorDirPin1  50    //Front Right Motor direction pin 1 to Model-Y M_B IN1  (K1)
 #define RightMotorDirPin2  48   //Front Right Motor direction pin 2 to Model-Y M_B IN2   (K1)                                 
 #define LeftMotorDirPin1  46    //Front Left Motor direction pin 1 to Model-Y M_B IN3 (K3)
 #define LeftMotorDirPin2  44   //Front Left Motor direction pin 2 to Model-Y M_B IN4 (K3)
-#define speedPinL 42   //  Front Wheel PWM pin connect Model-Y M_B ENB
+#define speedPinL 12   //  Front Wheel PWM pin connect Model-Y M_B ENB
 
-#define speedPinRB 32   //  Rear Wheel PWM pin connect Left Model-Y M_A ENA 
+#define speedPinRB 6   //  Rear Wheel PWM pin connect Left Model-Y M_A ENA 
 #define RightMotorDirPin1B  30    //Rear Right Motor direction pin 1 to Model-Y M_A IN1 ( K1)
 #define RightMotorDirPin2B 28    //Rear Right Motor direction pin 2 to Model-Y M_A IN2 ( K1) 
 #define LeftMotorDirPin1B 26    //Rear Left Motor direction pin 1 to Model-Y M_A IN3  (K3)
 #define LeftMotorDirPin2B 24  //Rear Left Motor direction pin 2 to Model-Y M_A IN4 (K3)
-#define speedPinLB 22    //  Rear Wheel PWM pin connect Model-Y M_A ENB
-
+#define speedPinLB 8    //  Rear Wheel PWM pin connect Model-Y M_A ENB
 
 /*motor control*/
-void go_advance(int speed){
-   FL_fwd(speed); 
-   FR_fwd(speed);
-   RL_fwd(speed);
-   RR_fwd(speed);
-
-}
-void go_back(int speed){
-   FL_bck(speed); 
-   FR_bck(speed);
-   RL_bck(speed);
-   RR_bck(speed);
-
-}
 void right_shift(int speed_fl_fwd,int speed_rl_bck ,int speed_rr_fwd,int speed_fr_bck) {
   FL_fwd(speed_fl_fwd); 
-  FR_bck(speed_fr_bck);
   RL_bck(speed_rl_bck); 
+  FR_bck(speed_fr_bck);
   RR_fwd(speed_rr_fwd);
-
+;
 }
 void left_shift(int speed_fl_bck,int speed_rl_fwd ,int speed_rr_bck,int speed_fr_fwd){
    FL_bck(speed_fl_bck);
-   FR_fwd(speed_fr_fwd);
    RL_fwd(speed_rl_fwd);
+   FR_fwd(speed_fr_fwd);
    RR_bck(speed_rr_bck);
+  
 }
-
-void left_turn(int speed){
-   FL_bck(0); 
+void go_advance(int speed){
+   RL_fwd(speed);
+   RR_fwd(speed);
    FR_fwd(speed);
+   FL_fwd(speed); 
+}
+void  go_back(int speed){
+   RL_bck(speed);
+   RR_bck(speed);
+   FR_bck(speed);
+   FL_bck(speed); 
+}
+void left_turn(int speed){
    RL_bck(0);
    RR_fwd(speed);
+   FR_fwd(speed);
+   FL_bck(0); 
 }
 void right_turn(int speed){
-  //  FL_fwd(speed); 
-  //  FR_bck(0);
-  //  RL_fwd(speed);
-  //  RR_bck(0);
-
-   FL_fwd(speed); 
-   FR_fwd(0);
    RL_fwd(speed);
-   RR_fwd(0);
-
+   RR_bck(0);
+   FR_bck(0);
+   FL_fwd(speed); 
 }
 void left_back(int speed){
-   FL_fwd(0); 
-   FR_bck(speed);
    RL_fwd(0);
    RR_bck(speed);
-
+   FR_bck(speed);
+   FL_fwd(0); 
 }
 void right_back(int speed){
-   FL_bck(speed); 
-   FR_fwd(0);
    RL_bck(speed);
    RR_fwd(0);
-
- 
+   FR_fwd(0);
+   FL_bck(speed); 
 }
+
 void clockwise(int speed){
-   FL_fwd(speed); 
-   FR_bck(speed);
    RL_fwd(speed);
    RR_bck(speed);
+   FR_bck(speed);
+   FL_fwd(speed); 
 }
 void countclockwise(int speed){
-   FL_bck(speed); 
-   FR_fwd(speed);
    RL_bck(speed);
    RR_fwd(speed);
+   FR_fwd(speed);
+   FL_bck(speed); 
 }
-
-void FL_fwd(int speed) // front-left wheel forward turn
+void FR_bck(int speed)  //front-right wheel forward turn
 {
-  digitalWrite(LeftMotorDirPin1,HIGH);
-  digitalWrite(LeftMotorDirPin2,LOW);
-  analogWrite(speedPinL,speed);
+  digitalWrite(RightMotorDirPin1, LOW);
+  digitalWrite(RightMotorDirPin2,HIGH); 
+  analogWrite(speedPinR,speed);
 }
-void FL_bck(int speed) // front-left wheel backward turn
-{
-  digitalWrite(LeftMotorDirPin1,LOW);
-  digitalWrite(LeftMotorDirPin2,HIGH);
-  analogWrite(speedPinL,speed);
-}
-
-void FR_fwd(int speed)  //front-right wheel forward turn
+void FR_fwd(int speed) // front-right wheel backward turn
 {
   digitalWrite(RightMotorDirPin1,HIGH);
   digitalWrite(RightMotorDirPin2,LOW); 
   analogWrite(speedPinR,speed);
 }
-void FR_bck(int speed) // front-right wheel backward turn
+void FL_bck(int speed) // front-left wheel forward turn
 {
-  digitalWrite(RightMotorDirPin1,LOW);
-  digitalWrite(RightMotorDirPin2,HIGH); 
-  analogWrite(speedPinR,speed);
+  digitalWrite(LeftMotorDirPin1,LOW);
+  digitalWrite(LeftMotorDirPin2,HIGH);
+  analogWrite(speedPinL,speed);
+}
+void FL_fwd(int speed) // front-left wheel backward turn
+{
+  digitalWrite(LeftMotorDirPin1,HIGH);
+  digitalWrite(LeftMotorDirPin2,LOW);
+  analogWrite(speedPinL,speed);
 }
 
-void RL_fwd(int speed)  //rear-left wheel forward turn
-{
-  digitalWrite(LeftMotorDirPin1B,HIGH);
-  digitalWrite(LeftMotorDirPin2B,LOW);
-  analogWrite(speedPinLB,speed);
-}
-void RL_bck(int speed)    //rear-left wheel backward turn
-{
-  digitalWrite(LeftMotorDirPin1B,LOW);
-  digitalWrite(LeftMotorDirPin2B,HIGH);
-  analogWrite(speedPinLB,speed);
-}
-
-void RR_fwd(int speed)  //rear-right wheel forward turn
-{
-  digitalWrite(RightMotorDirPin1B, HIGH);
-  digitalWrite(RightMotorDirPin2B,LOW); 
-  analogWrite(speedPinRB,speed);
-}
-void RR_bck(int speed)  //rear-right wheel backward turn
+void RR_bck(int speed)  //rear-right wheel forward turn
 {
   digitalWrite(RightMotorDirPin1B, LOW);
   digitalWrite(RightMotorDirPin2B,HIGH); 
   analogWrite(speedPinRB,speed);
 }
-
+void RR_fwd(int speed)  //rear-right wheel backward turn
+{
+  digitalWrite(RightMotorDirPin1B, HIGH);
+  digitalWrite(RightMotorDirPin2B,LOW); 
+  analogWrite(speedPinRB,speed);
+}
+void RL_bck(int speed)  //rear-left wheel forward turn
+{
+  digitalWrite(LeftMotorDirPin1B,LOW);
+  digitalWrite(LeftMotorDirPin2B,HIGH);
+  analogWrite(speedPinLB,speed);
+}
+void RL_fwd(int speed)    //rear-left wheel backward turn
+{
+  digitalWrite(LeftMotorDirPin1B,HIGH);
+  digitalWrite(LeftMotorDirPin2B,LOW);
+  analogWrite(speedPinLB,speed);
+}
+ 
  
 void stop_Stop()    //Stop
 {
-  analogWrite(speedPinLB,0);
+     analogWrite(speedPinLB,0);
   analogWrite(speedPinRB,0);
-  analogWrite(speedPinL,0);
+    analogWrite(speedPinL,0);
   analogWrite(speedPinR,0);
 }
 
@@ -181,73 +166,105 @@ void init_GPIO()
   pinMode(LeftMotorDirPin1, OUTPUT);
   pinMode(LeftMotorDirPin2, OUTPUT); 
   pinMode(speedPinR, OUTPUT);
-  
   pinMode(RightMotorDirPin1B, OUTPUT); 
   pinMode(RightMotorDirPin2B, OUTPUT); 
   pinMode(speedPinLB, OUTPUT);  
  
   pinMode(LeftMotorDirPin1B, OUTPUT);
-  pinMode(LeftMotorDirPin2B, OUTPUT);
+  pinMode(LeftMotorDirPin2B, OUTPUT); 
   pinMode(speedPinRB, OUTPUT);
    
   stop_Stop();
 }
+#include "WiFiEsp.h"
+#include "WiFiEspUDP.h"
+char ssid[] = "osoyoo_robot"; 
 
+int status = WL_IDLE_STATUS;
+// use a ring buffer to increase speed and reduce memory allocation
+ char packetBuffer[5]; 
+WiFiEspUDP Udp;
+unsigned int localPort = 8888;  // local port to listen on
 void setup()
 {
-  init_GPIO();
-  delay(4000);
- 
-go_advance(SPEED);
-     delay(800);
-     stop_Stop();
-     delay(800);
-  
-go_back(SPEED);
-      delay(800);
-      stop_Stop();
-      delay(800);
-	  
-left_turn(TURN_SPEED);
-      delay(800);
-      stop_Stop();
-      delay(800);
-	  
-right_turn(TURN_SPEED);
-     delay(800);
-     stop_Stop();
-     delay(800);
-	   
-left_shift(200,200,200,200); //left shift
-     delay(800);
-     stop_Stop();
-     delay(800);	
-	 
-right_shift(200,200,200,200); //right shift
-     delay(800);
-     stop_Stop();
-     delay(800);
-	  
-right_shift(200,0,200,0); //right diagonal ahead
-     delay(800);
-     stop_Stop();
-	 delay(800);	 
-	 
-left_shift(200,0,200,0); //left diagonal back
-     delay(800);
-     stop_Stop();
-	 delay(800);
- 
-left_shift(0,200,0,200); //left diagonal ahead
-     delay(800);
-     stop_Stop();
-     delay(800);
+ init_GPIO();
+  Serial.begin(9600);   // initialize serial for debugging
+    Serial1.begin(115200);
+    Serial1.write("AT+UART_DEF=9600,8,1,0,0\r\n");
+  delay(200);
+  Serial1.write("AT+RST\r\n");
+  delay(200);
+  Serial1.begin(9600);    // initialize serial for ESP module
+  WiFi.init(&Serial1);    // initialize ESP module
 
-right_shift(0,200,0,200); //right diagonal back
-     delay(800);
-     stop_Stop();
-	 delay(800);
+  // check for the presence of the shield
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue
+    while (true);
+  }
+
+    Serial.print("Attempting to start AP ");
+   Serial.println(ssid);
+   //AP mode
+   status = WiFi.beginAP(ssid, 10, "", 0);
+
+  Serial.println("You're connected to the network");
+  printWifiStatus();
+  Udp.begin(localPort);
+  
+  Serial.print("Listening on port ");
+  Serial.println(localPort);
 }
 
-void loop(){
+
+void loop()
+{
+  int packetSize = Udp.parsePacket();
+  if (packetSize) {                               // if you get a client,
+     Serial.print("Received packet of size ");
+    Serial.println(packetSize);
+    int len = Udp.read(packetBuffer, 255);
+    if (len > 0) {
+      packetBuffer[len] = 0;
+    }
+      char c=packetBuffer[0];
+      switch (c)    //serial control instructions
+      {  
+        case 'A':go_advance(SPEED);;break;
+        case 'L':left_turn(TURN_SPEED);break;
+        case 'R':right_turn(TURN_SPEED);break;
+        case 'B':go_back(SPEED);break;
+        case 'E':stop_Stop();break;
+        case 'F':left_shift(0,SPEED,0,SPEED);break; //left ahead
+        case 'H':right_shift(SPEED,0,SPEED,0);break; //right ahead
+        case 'I':left_shift(SPEED,0,SPEED,0); break;//left back
+        case 'K':right_shift(0,SPEED,0,SPEED); break;//right back
+        //case 'O':left_shift(200,150,150,200); break;//left shift
+        //case 'T':right_shift(200,200,200,200); break;//left shift
+        default:break;
+      }
+    }
+    
+}
+
+
+ 
+
+void printWifiStatus()
+{
+  // print the SSID of the network you're attached to
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print where to go in the browser
+  Serial.println();
+  Serial.print("To see this page in action, open a browser to http://");
+  Serial.println(ip);
+  Serial.println();
 }
