3d2e2d8fa3807557d0a2c75bb05b06c605f82837
jegert0 <jegertm@gmail.com>
2024-02-07 21:33:02 -0500

    updated inputScence for emotiv

--- Unity BCI Car/Assets/Scripts/ArrowShower.cs
+++ Unity BCI Car/Assets/Scripts/ArrowShower.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using EmotivUnityPlugin;
 
 public class ArrowShower : MonoBehaviour
-{
+{   
+    EmotivUnityItf _eItf = EmotivUnityItf.Instance;
+    public bool headset = false;
     [SerializeField] RawImage leftArrow;
     [SerializeField] RawImage rightArrow;
     [SerializeField] RawImage upArrow;
@@ -17,32 +20,67 @@ public class ArrowShower : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {   //detects key input and makes arrow visable
-        if (Input.GetKey(KeyCode.UpArrow)){
-            upArrow.enabled = true;
+    {   if(!headset){
+            //detects key input and makes arrow visable
+            if (Input.GetKey(KeyCode.UpArrow)){
+                upArrow.enabled = true;
+            }
+            else{
+                upArrow.enabled = false;
+            }
+            if(Input.GetKey(KeyCode.DownArrow)){
+                downArrow.enabled = true;
+            }
+            else{
+                downArrow.enabled = false;
+            }
+            if(Input.GetKey(KeyCode.LeftArrow)){
+                leftArrow.enabled = true;
+            }
+            else{
+                leftArrow.enabled = false;
+            }
+            if (Input.GetKey(KeyCode.RightArrow)){
+                rightArrow.enabled = true;
+            }
+            else{
+                rightArrow.enabled = false;
+            }
         }
-        else{
-            upArrow.enabled = false;
-        }
-        if(Input.GetKey(KeyCode.DownArrow)){
-            downArrow.enabled = true;
-        }
-        else{
-            downArrow.enabled = false;
-        }
-        if(Input.GetKey(KeyCode.LeftArrow)){
-            leftArrow.enabled = true;
-        }
-        else{
-            leftArrow.enabled = false;
-        }
-        if (Input.GetKey(KeyCode.RightArrow)){
-            rightArrow.enabled = true;
-        }
-        else{
-            rightArrow.enabled = false;
-        }
+        else headsetParser_Input();
         
         
+    }
+
+    void headsetParser_Input(){
+        if(_eItf != null)
+        {
+            if(_eItf.LatestMentalCommand.act != "NULL")
+            {   //need to use if else statements so when command switches to other command arrow clears
+                string current_command = _eItf.LatestMentalCommand.act;
+                if (current_command == "push")
+                    upArrow.enabled = true;
+                
+                else upArrow.enabled = false;
+            
+                if(current_command == "pull")
+                    downArrow.enabled = true;
+                
+                else downArrow.enabled = false;
+                
+                if(current_command == "left")
+                    leftArrow.enabled = true;
+                
+                else leftArrow.enabled = false;
+                
+                if (current_command == "right")
+                    rightArrow.enabled = true;
+                
+                else rightArrow.enabled = false;
+                
+
+                
+            }
+        }
     }
 }
